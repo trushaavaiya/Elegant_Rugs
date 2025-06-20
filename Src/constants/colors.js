4e610d869392b04792bf2a2f690f06a57326442e
Trusha Avaiya <trushaavaiya@gmail.com>
2025-06-20 18:05:18 +0530

    added color for splash screen2

--- Src/constants/colors.js
+++ Src/constants/colors.js
@@ -2,7 +2,8 @@ const Colors = {
   primary: '#B56A3F',     // For "RUGS" text color
   black: '#000000',
   white: '#FFFFFF',
-  background: '#FFFFFF',
+  grey:'#666',
+  white2:'#F5F0EC',
 };
 
 export default Colors;
