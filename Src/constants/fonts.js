6f51a8b5c09c9ea8153a5925bd1282f136918fbc
Trusha Avaiya <trushaavaiya@gmail.com>
2025-06-20 17:24:20 +0530

    Added Fonts

---
+++ Src/constants/fonts.js
@@ -0,0 +1,11 @@
+const Fonts = {
+  regular: 'System',         // default system font
+  bold: 'System',            // or use a custom font like 'Roboto-Bold'
+  size: {
+    small: 14,
+    medium: 18,
+    large: 24,
+  },
+};
+
+export default Fonts;
